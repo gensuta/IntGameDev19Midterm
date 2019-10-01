519d558a340d95e2353534c96593c7e854cf256c
Geneva Heyward <geneva.heyward@urbanarts.org>
2019-10-01 15:07:42 -0400

    Just setting some things up ( adding in more *blank* scripts that I will fill in along with empty scenes and such) Created some more folders to organize scriptable objects, 3D models, and other things that will be added soon.
    Began creating the hallway and positioning the player. Will begin working on the scriptable objects/classes next and THEN I'll place the other characters that will be fought next

---
+++ IntGameDevMidterm_Fall19/Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    public AudioSource aud;
+
+    [Space]
+    [Header("Bools")]
+    public bool didStart;
+    public bool fadingIn;
+    public bool hasLastBeatPassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadingIn)
+        {
+            if (aud.volume < 1f)
+            {
+                aud.volume += 0.3f * Time.deltaTime;
+            }
+            else
+            {
+                fadingIn = false;
+            }
+        }
+        else
+        {
+            aud.volume = 1f;
+        }
+    }
+
+    public void SwitchSong(AudioClip song, bool isLooping = true)
+    {
+        fadingIn = false;
+        aud.clip = song;
+        aud.loop = isLooping;
+
+        aud.Play();
+    }
+    public void FadeIn(AudioClip song)
+    {
+        aud.Stop();
+        aud.time += 30f;
+        aud.volume = 0f;
+        aud.clip = song;
+        aud.Play();
+        fadingIn = true;
+
+    }
+}
--- IntGameDevMidterm_Fall19/Assets/Scripts/Controllers/BattleHandler.cs
+++ IntGameDevMidterm_Fall19/Assets/Scripts/Controllers/BattleHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioController : MonoBehaviour
+public class BattleHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
